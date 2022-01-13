cb7ff226c8500bcd64c152e63e50e4ff84f65707
Ethan Lee <devohno.ko@gmail.com>
2022-01-14 00:20:51 +0900

    Add a `header` component sample

---
+++ src/components/header.component.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+import { IHeader } from './interface';
+
+/**
+ * 헤더
+ */
+export const Header: FC<IHeader> = ({ back = true, title }) => {
+    return <div>Header</div>;
+};
