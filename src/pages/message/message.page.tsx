12cbded52092bdfd57dd630e97575ff1eacf11f8
Ethan Lee <55861805+devethan@users.noreply.github.com>
2022-01-17 23:56:49 +0900

    컴포넌트 인터페이스 및 스키마 작성 (#4)
    * Setup code format by prettier
    
    - run `yarn prettier` to re-format all of codes
    
    * Add a model & interfaces
    
    * Add a `header` component sample

--- src/pages/message/message.page.tsx
+++ src/pages/message/message.page.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MessagePage = (): JSX.Element => {
-  return (
-    <div>
-      <h1>MESSAGE PAGE</h1>
-      <Link to="/loading">저장</Link>
-    </div>
-  );
+    return (
+        <div>
+            <h1>MESSAGE PAGE</h1>
+            <Link to="/loading">저장</Link>
+        </div>
+    );
 };
 
 export default MessagePage;
