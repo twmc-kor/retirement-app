0d9a7b5ec3da55e478e5a8599164ad3d40d33f1b
deveassy <xjfmwld@gmail.com>
2022-01-19 01:13:15 +0900

    add header component in user-styling page

--- src/pages/user-styling/user-styling.page.tsx
+++ src/pages/user-styling/user-styling.page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Header } from '../../components';
+import { Wrapper } from '../../components/styles';
 
 const UserStylingPage = (): JSX.Element => {
     return (
-        <div>
-            <h1>USER-STYLING PAGE</h1>
+        <Wrapper>
+            <Header back title="꽃길레터" />
             <Link to="/message">캐릭터</Link>
-        </div>
+        </Wrapper>
     );
 };
 
