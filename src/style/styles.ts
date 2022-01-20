b2625f78b8483177f429e8962216e143cd6d1e2c
deveassy <xjfmwld@gmail.com>
2022-01-21 01:15:20 +0900

    feat: add style type

--- src/style/styles.ts
+++ src/style/styles.ts
@@ -1,6 +1,20 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+@font-face {
+  font-family: 'ONE-Mobile-POP';
+  src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
+
+@font-face {
+    font-family: 'KOTRAHOPE';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2110@1.0/KOTRAHOPE.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+
   body {
     max-width: 360px;
     margin: 0 auto;
@@ -11,18 +25,102 @@ const GlobalStyle = createGlobalStyle`
 
 export default GlobalStyle;
 
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100vh;
-`;
+export const Styles: IStyles = {
+    /** 폰트 프로토타입 */
+    FONT: {
+        TITLE_NAME: {
+            'font-family': 'KOTRAHOPE',
+            'font-size': '26px',
+            'font-weight': '400',
+            'line-height': '26px',
+        },
+        PAGE_TEXT: {
+            'font-family': 'ONE-Mobile-POP',
+            'font-size': '26px',
+            'font-weight': '400',
+            'line-height': '24px',
+        },
+        MODAL_TEXT: {
+            'font-family': 'KOTRAHOPE',
+            'font-size': '26px',
+            'font-weight': '400',
+            'line-height': '30px',
+        },
+        PAGE_SUB_TEXT: {
+            'font-family': 'ONE-Mobile-POP',
+            'font-size': '20px',
+            'font-weight': '400',
+            'line-height': '26px',
+        },
+        SUB_TEXT: {
+            'font-family': 'KOTRAHOPE',
+            'font-size': '20px',
+            'font-weight': '400',
+            'line-height': '18px',
+        },
 
-// @font-face {
-//   font-family: 'ONE-Mobile-POP';
-//   src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/ONE-Mobile-POP.woff') format('woff');
-//   font-weight: normal;
-//   font-style: normal;
-// }
+        MESSAGE_TEXT: {
+            'font-family': 'KOTRAHOPE',
+            'font-size': '18px',
+            'font-weight': '400',
+            'line-height': '22px',
+        },
+
+        USER_NAME: {
+            'font-family': 'KOTRAHOPE',
+            'font-size': '16px',
+            'font-weight': '400',
+            'line-height': '24px',
+        },
+        NOTI_TEXT: {
+            'font-family': 'KOTRAHOPE',
+            'font-size': '14px',
+            'font-weight': '400',
+            'line-height': '24px',
+        },
+    },
+    /** 색상 프로토타입 */
+    COLOR: {
+        HEADER: '#C2DBFF',
+        MAIN_TEXT: '#693F08',
+        POINT: '#FF6F50',
+        DISABLE: '#C5BCB8',
+        MAIN_BUTTON: '#834E00',
+        SUB_BUTTON: '#88ADFF',
+        NOTI: '#48BD70',
+        TEXT: '#8A8A8A',
+        WHITE: '#ffffff',
+    },
+};
+
+export type IStyles = {
+    FONT: {
+        [K in FontKeys]: {
+            [key: string]: string;
+        };
+    };
+    COLOR: {
+        [K in ColorKeys]: string;
+    };
+};
+
+type FontKeys =
+    | 'TITLE_NAME'
+    | 'PAGE_TEXT'
+    | 'MODAL_TEXT'
+    | 'PAGE_SUB_TEXT'
+    | 'SUB_TEXT'
+    | 'MESSAGE_TEXT'
+    | 'USER_NAME'
+    | 'NOTI_TEXT';
+
+type ColorKeys =
+    | 'HEADER'
+    | 'MAIN_TEXT'
+    | 'POINT'
+    | 'DISABLE'
+    | 'MAIN_BUTTON'
+    | 'SUB_BUTTON'
+    | 'NOTI'
+    | 'TEXT'
+    | 'WHITE';
