c886d7f172b9fb88f6eaf22b1f64fb3f3ee027bb
deveassy <xjfmwld@gmail.com>
2022-01-22 23:43:13 +0900

    change index page style

--- src/pages/index/index.page.tsx
+++ src/pages/index/index.page.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { Container, DefaultButton } from '../../components/styles';
+import { Styles } from '../../style/styles';
 
 const IndexPage = (): JSX.Element => {
+    const navigation = useNavigate();
+
+    const handleEntrance = () => {
+        navigation('/onboarding');
+    };
     return (
-        <div>
-            <h1>INDEX PAGE</h1>
-            <Link to="/class-room">입장하기</Link>
-        </div>
+        <Container>
+            <Title>꽃길 레터</Title>
+            <Img src="/img/ex1.png" />
+            <Button onClick={handleEntrance}>입장하기</Button>
+        </Container>
     );
 };
 
 export default IndexPage;
+
+const Title = styled.span`
+    margin: 56px 0 20px;
+    font-family: 'KOTRAHOPE';
+    font-size: 45px;
+`;
+
+const Img = styled.img`
+    width: 230px;
+    height: 300px;
+`;
+
+const Button = styled(DefaultButton)`
+    width: 150px;
+    height: 40px;
+    margin-top: 20px;
+    background-color: ${Styles.COLOR.HEADER};
+    ${Styles.FONT.PAGE_SUB_TEXT};
+    color: ${Styles.COLOR.WHITE};
+`;
